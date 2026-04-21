b47ba637689f18886e0f8aa52520131aead83fad
EmilioArias <emilio.arias@fractaliasystems.es>
2026-04-20 20:40:43 -0500

    Unit Test

---
+++ UnitTest/CharacterServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using PruebaTecnicaKibernum.Application.Dtos.ApiDto;
+using PruebaTecnicaKibernum.Application.Interfaces;
+using PruebaTecnicaKibernum.Application.Services;
+using PruebaTecnicaKibernum.Domain.Entities;
+
+namespace UnitTest
+{
+    public class CharacterServiceTests
+    {
+        private readonly Mock<ICharacterRepository> _RepoMock = new();
+        private readonly Mock<IRickAndMortyService> _ApiMock = new();
+        private readonly Mock<ILogger<CharacterService>> _LoggerMock = new();
+
+        [Fact]
+        public async Task ImportCharacterAsync_ShouldAddNewCharacters_WhenNotExists()
+        {
+            //Arrange
+            var lService = new CharacterService(_RepoMock.Object, _ApiMock.Object, _LoggerMock.Object);
+
+            _ApiMock.Setup(x => x.GetCharactersAsync(It.IsAny<int>()))
+                .ReturnsAsync(new RickAndMortyResponseDto
+                {
+                    Info = new InfoDto { Pages = 1 },
+                    Results =
+                    [
+                        new() 
+                        { 
+                            Id = 1, 
+                            Name = "Rick", 
+                            Status = "Alive",
+                            Species = "Human",
+                            Gender = "Male",
+                            Image = "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+                            Origin = new OriginDto
+                            {
+                                Name = "Earth (C-137)"
+                            }
+                        }
+                    ]
+                });
+
+            _RepoMock.Setup(x => x.ExistsByExternalIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(false);
+
+            //Act
+            await lService.ImportCharacterAsync();
+
+            //Assert
+            _RepoMock.Verify(x => x.AddAsync(It.IsAny<Character>()), Times.Once);
+            _RepoMock.Verify(x => x.SaveChangesAsync(), Times.AtLeastOnce);
+        }
+    }
+}
---
+++ UnitTest/HiringServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using PruebaTecnicaKibernum.Application.Dtos.HiringDto;
+using PruebaTecnicaKibernum.Application.Interfaces;
+using PruebaTecnicaKibernum.Application.Services;
+using PruebaTecnicaKibernum.Domain.Entities;
+using PruebaTecnicaKibernum.Domain.Enums;
+
+namespace UnitTest
+{
+    public class HiringServiceTests
+    {
+        private readonly Mock<IHiringRequestRepository> _RepoMock = new();
+        private readonly Mock<ICharacterRepository> _CharRepoMock = new();
+        private readonly Mock<ILogger<HiringRequestService>> _LoggerMock = new();
+
+        [Fact]
+        public async Task CreateAsync_ShouldCreateRequest_WhenCharacterExists()
+        {
+            // Arrange
+            var lService = new HiringRequestService(_CharRepoMock.Object, _RepoMock.Object, _LoggerMock.Object);
+
+            _CharRepoMock.Setup(x => x.ExistsByExternalIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(true);
+
+            var dto = new CreateHiringRequestDto
+            {
+                CharacterId = 1,
+                Applicant = "Netflix",
+                Event = "Serie",
+                EventDate = DateTime.UtcNow.AddDays(1)
+            };
+
+            // Act
+            var lResult = await lService.CreateAsync(dto);
+
+            // Assert
+            _RepoMock.Verify(x => x.AddAsync(It.IsAny<HiringRequest>()), Times.Once);
+            _RepoMock.Verify(x => x.SaveChangesAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task UpdateStatusAsync_ShouldUpdateStatus_WhenRequestExists()
+        {
+            // Arrange
+            var lRequest = new HiringRequest
+            {
+                Id = 1,
+                Status = RequestStatus.PENDING
+            };
+
+            _RepoMock.Setup(x => x.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(lRequest);
+
+            var service = new HiringRequestService(_CharRepoMock.Object, _RepoMock.Object, _LoggerMock.Object);
+
+            // Act
+            await service.UpdateStatusAsync(1, RequestStatus.APPROVED);
+
+            // Assert
+            lRequest.Status.Should().Be(RequestStatus.APPROVED);
+            _RepoMock.Verify(x => x.SaveChangesAsync(), Times.Once);
+        }
+    }
+}
